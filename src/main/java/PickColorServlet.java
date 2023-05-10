c43132759452aa1419c8d853429bc62b8adba415
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-05-10 14:12:07 -0500

    completed servlets-with-jsps exercise

--- src/main/java/PickColorServlet.java
+++ src/main/java/PickColorServlet.java
@@ -5,7 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/pickcolor")ad
+@WebServlet("/pickcolor")
 public class PickColorServlet extends HttpServlet {
 
     @Override
