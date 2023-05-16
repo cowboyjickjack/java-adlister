f32eca51de3472212dd34935aa1633f663d2fe79
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-05-16 13:43:54 -0500

    finished user-input lecture

--- src/main/java/dao/MySQLAdsDao.java
+++ src/main/java/dao/MySQLAdsDao.java
@@ -12,54 +12,62 @@ public class MySQLAdsDao implements Ads {
 
     private Connection connection;
 
-    public MySQLAdsDao() throws SQLException { // this is essentially making that ^^ connection work
-        DriverManager.registerDriver(new Driver());
-        connection = DriverManager.getConnection( // we will use this connection to access the correct db
-                Config.getUrl(),
-                Config.getUser(),
-                Config.getPassword()
-        );
+    public MySQLAdsDao() {
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
+                    Config.getUrl(),
+                    Config.getUser(),
+                    Config.getPassword()
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException("Error connecting to the database!", e);
+        }
     }
 
     @Override
     public List<Ad> all() {
-        List<Ad> ads = new ArrayList<>();
+        Statement stmt = null;
         try {
-            // Establish connection
-            Statement statement = connection.createStatement();
-            // Selecting everything from our ads table
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM ads");
-            while (resultSet.next()) {
-                Ad ad = new Ad(
-                        1, // for now, we'll hardcode the user id
-                        resultSet.getLong("user_id"),
-                        resultSet.getString("title"),
-                        resultSet.getString("description")
-                );
-                // NOW add it to the list!
-                ads.add(ad);
-            }
+            stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM ads");
+            return createAdsFromResults(rs);
         } catch (SQLException e) {
-            System.out.println("SQL Exception: " + e);
+            throw new RuntimeException("Error retrieving all ads.", e);
         }
-        return ads;
     }
 
     @Override
-    public Long insert(Ad ad) { // Getting Ad object and adding it to our db
+    public Long insert(Ad ad) {
+        String query = "INSERT INTO ads(user_id, title, description) VALUES (?, ?, ?)";
         try {
-            Statement statement = connection.createStatement();
-            // Now updating the ads table
-            statement.executeUpdate("INSERT INTO ads (user_id, title, description) VALUES (" + ad.getUserId() + ", '" + ad.getTitle() + "', '" + ad.getDescription() + "')", Statement.RETURN_GENERATED_KEYS);
-            ResultSet resultSet = statement.getGeneratedKeys();
-            // if resultSet is real/exists, continue
-            if (resultSet.next()) {
-                return resultSet.getLong(1);
-            }
+            PreparedStatement stmt = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            stmt.setLong(1, ad.getUserId());
+            stmt.setString(2, ad.getTitle());
+            stmt.setString(3, ad.getDescription());
+            stmt.executeUpdate();
+            ResultSet rs = stmt.getGeneratedKeys();
+            rs.next();
+            return rs.getLong(1);
         } catch (SQLException e) {
-            System.out.println("SQL Exception: " + e);
+            throw new RuntimeException("Error creating a new ad.", e);
         }
-        // return null in case it catches the exception :)
-        return null;
+    }
+
+    private Ad extractAd(ResultSet rs) throws SQLException {
+        return new Ad(
+                rs.getLong("id"),
+                rs.getLong("user_id"),
+                rs.getString("title"),
+                rs.getString("description")
+        );
+    }
+
+    private List<Ad> createAdsFromResults(ResultSet rs) throws SQLException {
+        List<Ad> ads = new ArrayList<>();
+        while (rs.next()) {
+            ads.add(extractAd(rs));
+        }
+        return ads;
     }
 }
