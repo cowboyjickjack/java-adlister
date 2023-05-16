9faa7e96f0e042ea527e8bb98a101812a9bbb5aa
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-05-16 11:39:13 -0500

    completed jdbc walkthrough

--- src/main/java/dao/DaoFactory.java
+++ src/main/java/dao/DaoFactory.java
@@ -1,12 +1,14 @@
 package dao;
 
 
+import java.sql.SQLException;
+
 public class DaoFactory {
     private static Ads adsDao;
 
-    public static Ads getAdsDao() {
+    public static Ads getAdsDao() throws SQLException {
         if (adsDao == null) {
-            adsDao = new ListAdsDao();
+            adsDao = new MySQLAdsDao();
         }
         return adsDao;
     }
