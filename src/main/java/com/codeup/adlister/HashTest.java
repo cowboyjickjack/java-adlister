5076466a71e9ec009544ef9aba9f0caaea872cae
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-05-17 16:33:48 -0500

    added BCrypt dependency

--- src/main/java/com/codeup/adlister/HashTest.java
+++ src/main/java/com/codeup/adlister/HashTest.java
@@ -5,8 +5,17 @@ import org.mindrot.jbcrypt.BCrypt;
 public class HashTest {
 
     public static void main(String[] args) {
+        String password = "123456";
+        // SALTING adds random characters
         String salt = BCrypt.gensalt();
         System.out.println(salt);
+        // HASHING then converts it to consistent ciphertext
+        String hash = BCrypt.hashpw(password, salt);
+        System.out.println(hash);
+
+        // CHECK PASSWORD
+        boolean passwordsMatch = BCrypt.checkpw(password, hash);
+        System.out.println(passwordsMatch);
     }
 
 }
